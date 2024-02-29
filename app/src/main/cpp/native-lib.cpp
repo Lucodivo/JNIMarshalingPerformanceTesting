c5cc529e219633c9b2d7706daae43cd15a0b56ef
Connor Haskins <ConnorAHaskins@gmail.com>
2024-02-29 01:36:20 -0800

    iterative timing

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,34 +7,33 @@ global_variable u64 estimateCPUFrequency;
 global_variable u64 startClocks;
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject thiz) {
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_stringFromJNI(JNIEnv* env, jclass) {
     return env->NewStringUTF("Hello from C++");
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_initialize(JNIEnv *env, jobject thiz){
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_initialize(JNIEnv*, jclass){
   estimateCPUFrequency = EstimateCPUTimerFreq(1000);
 }
+
 extern "C" JNIEXPORT void JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_startTime(JNIEnv *env, jobject thiz){
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_startTime(JNIEnv*, jclass){
   startClocks = ReadCPUTimer();
-}
-extern "C" JNIEXPORT jdouble JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_endTime(JNIEnv *env, jobject thiz){
-  u64 endClocks = ReadCPUTimer();
-  u64 totalClocks = endClocks - startClocks;
-  jdouble totalSeconds = (double)totalClocks / estimateCPUFrequency;
-  return totalSeconds;
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sumC(
-      JNIEnv* env,
-      jobject thiz,
-      jintArray javaIntArrayPtr
-    ){
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_endTime(JNIEnv*, jclass){
+  u64 endClocks = ReadCPUTimer();
+  return endClocks - startClocks;
+}
+
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_clocksToSeconds(JNIEnv*, jclass, jint clocks){
+  return (double)clocks / (double)estimateCPUFrequency;
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sumC(JNIEnv* env, jclass, jintArray javaIntArrayPtr){
   jsize size = env->GetArrayLength(javaIntArrayPtr);
   jint* javaIntArray = new jint[size];
   env->GetIntArrayRegion(javaIntArrayPtr, jsize{0}, size, javaIntArray);
@@ -47,11 +46,7 @@ Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sumC(
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sortC(
-    JNIEnv* env,
-    jobject thiz,
-    jintArray javaIntArrayPtr
-){
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sortC(JNIEnv* env, jclass, jintArray javaIntArrayPtr){
   jsize size = env->GetArrayLength(javaIntArrayPtr);
   jint* body = env->GetIntArrayElements(javaIntArrayPtr, NULL);
   std::sort(body, body + size);
@@ -59,13 +54,9 @@ Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_sortC(
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_plusOneC(
-    JNIEnv* env,
-    jobject thiz,
-    jintArray javaIntArrayPtr
-){
+Java_com_inasweaterpoorlyknit_jniplayground_MainActivity_plusOneC(JNIEnv* env, jclass, jintArray javaIntArrayPtr){
   jsize size = env->GetArrayLength(javaIntArrayPtr);
   jint* body = env->GetIntArrayElements(javaIntArrayPtr, NULL);
-  for(jsize i = 0; i < size; i++){ body[0] += 1; }
+  for(jsize i = 0; i < size; i++){ body[i] += 1; }
   env->ReleaseIntArrayElements(javaIntArrayPtr, body, 0);
 }
--- app/src/main/java/com/inasweaterpoorlyknit/jniplayground/MainActivity.kt
+++ app/src/main/java/com/inasweaterpoorlyknit/jniplayground/MainActivity.kt
@@ -3,83 +3,131 @@ package com.inasweaterpoorlyknit.jniplayground
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.inasweaterpoorlyknit.jniplayground.databinding.ActivityMainBinding
+import androidx.lifecycle.lifecycleScope
+import dalvik.annotation.optimization.CriticalNative
+import dalvik.annotation.optimization.FastNative
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.math.min
 import kotlin.random.Random
 
 val DEBUG_LOG_TAG = "jni_playground"
 fun logd(msg: String) = Log.d(DEBUG_LOG_TAG, msg)
 
-val MAX_PRINT_SIZE = 65
-fun logTimeHeader() {
-    val toPrint = CharArray(MAX_PRINT_SIZE){' '}
-    "=== Title ===".forEachIndexed { i, c ->
-        toPrint[i] = c
-    }
-    "=== Seconds ===".forEachIndexed { i, c ->
-        toPrint[30 + i] = c
-    }
-    Log.d(DEBUG_LOG_TAG, toPrint.joinToString(""))
-}
-fun logTime(title: String, seconds: Double) {
-    val toPrint = CharArray(MAX_PRINT_SIZE){' '}
-    for(i in title.indices){
-        if(i == 30) break
-        toPrint[i] = title[i]
-    }
-    seconds.toString().let{
-        it.substring(0, min(30, it.length)).forEachIndexed { i, c ->
-            toPrint[i + 30] = c
-        }
-    }
-    Log.d(DEBUG_LOG_TAG, toPrint.joinToString(""))
-}
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initialize()
-        val rand = Random(123)
-        logTimeHeader()
-
-        // timer overhead
-        val timerOverheadDuration = time{}
-        logTime("C Timer overhead", timerOverheadDuration)
-
-        val numbersKotlin = IntArray(100_000) { rand.nextInt() }
-        val numbersC = numbersKotlin.copyOf()
-
-        // plus one in-place Kotlin
-        val plusOneInPlaceKotlinDuration = time{ for(i in numbersKotlin.indices){ numbersKotlin[i] += 1 }}
-        logTime("Plus one in-place Kotlin", plusOneInPlaceKotlinDuration)
-
-        // plus one map Kotlin
-        var result: List<Int>
-        val plusOneCopyMapKotlinDuration = time{ result = numbersKotlin.map { it + 1 } }
-        logTime("Plus one copy (map) Kotlin",  plusOneCopyMapKotlinDuration)
-
-        // plus one C
-        val plusOneCDuration = time{ plusOneC(numbersC) }
-        logTime("Plus one C",  plusOneCDuration)
-
-        // default sort in Kotlin
-        val sortInKotlinDuration = time{ numbersKotlin.sort() }
-        logTime("Sorting numbers in Kotlin",  sortInKotlinDuration)
-
-        // default sort in C
-        val sortInCDuration = time{ sortC(numbersC) }
-        logTime("Sorting numbers in C",  sortInCDuration)
-
-        binding.sampleText.text = numbersC.take(100).printable()
+        performanceTests()
     }
 
-    private fun time(work: () -> Unit): Double { startTime(); work(); return endTime() }
+    fun logTimeHeader() {
+        val toPrint = CharArray(MAX_PRINT_SIZE){' '}
+        "=== Title ===".forEachIndexed { i, c ->
+            toPrint[i] = c
+        }
+        "=== Seconds ===".forEachIndexed { i, c ->
+            toPrint[40 + i] = c
+        }
+        Log.d(DEBUG_LOG_TAG, toPrint.joinToString(""))
+    }
+
+    fun logTime(title: String, clocks: Int) {
+        val toPrint = CharArray(MAX_PRINT_SIZE){' '}
+        for(i in title.indices){
+            if(i == 40) break
+            toPrint[i] = title[i]
+        }
+        clocksToSeconds(clocks).toString().let{
+            it.substring(0, min(40, it.length)).forEachIndexed { i, c ->
+                toPrint[i + 40] = c
+            }
+        }
+        Log.d(DEBUG_LOG_TAG, toPrint.joinToString(""))
+    }
+
+    fun TimedWork.log(title: String) {
+        logTime("$title (min)", minSecs)
+        logTime("$title (max)", maxSecs)
+        logd("$title (iterations): $totalIterations")
+    }
+
+    private fun performanceTests() {
+        lifecycleScope.launch(Dispatchers.Default) {
+            initialize()
+            val rand = Random(123)
+            logTimeHeader()
+
+            // timer overhead
+            val timerOverheadDuration = iterationTiming{}
+            timerOverheadDuration.log("C Timer Overhead")
+
+            val randomNumbers = IntArray(1_000_000) { rand.nextInt() }
+            val numbersCopy: () -> IntArray = { randomNumbers.copyOf() }
+
+            // plus one C
+            val numbersCPlusOne = numbersCopy()
+            val plusOneCDuration = iterationTiming{ plusOneC(numbersCPlusOne) }
+            plusOneCDuration.log("+1 C")
+
+            // plus one in-place Kotlin
+            val numbersKotlinInPlacePlusOne = numbersCopy()
+            val plusOneInPlaceKotlinDuration = iterationTiming{ for(i in numbersKotlinInPlacePlusOne.indices){ numbersKotlinInPlacePlusOne[i] += 1 } }
+            plusOneInPlaceKotlinDuration.log("+1 in-place Kotlin")
+
+            // plus one map Kotlin
+            var numbersKotlinMapPlusOne: List<Int>
+            val plusOneCopyMapKotlinDuration = iterationTiming{
+                numbersKotlinMapPlusOne = randomNumbers.map { it + 1 }
+            }
+            plusOneCopyMapKotlinDuration.log("+1 copy (map) Kotlin")
+
+            // default sort in Kotlin
+            val sortInKotlinDuration = iterationTiming(setup = numbersCopy){ it.sort() }
+            sortInKotlinDuration.log("Sorting in Kotlin")
+
+            // default sort in C
+            val sortInCDuration = iterationTiming(setup = numbersCopy){ sortC(it) }
+            sortInCDuration.log("Sorting in C")
+        }
+    }
+
+    private fun time(work: () -> Unit): Int { startTime(); work(); return endTime() }
+
+    data class TimedWork(
+        val minSecs: Int,
+        val maxSecs: Int,
+        val totalIterations: Int,
+    )
+
+    private fun iterationTiming(maxIterationsNoChange: Int = 10, work: () -> Unit): TimedWork {
+        var iterationsSinceChange = 0
+        var totalIterations = 0
+        var minSecs = Int.MAX_VALUE
+        var maxSecs = Int.MIN_VALUE
+        while(iterationsSinceChange < maxIterationsNoChange) {
+            val secs = time(work)
+            iterationsSinceChange += 1; totalIterations += 1;
+            if(secs < minSecs) { minSecs = secs; iterationsSinceChange = 0; }
+            if(secs > maxSecs) { maxSecs = secs; iterationsSinceChange = 0; }
+        }
+        return TimedWork(minSecs, maxSecs, totalIterations)
+    }
+
+    private fun <T> iterationTiming(maxIterationsNoChange: Int = 10, setup: () -> T, work: (T) -> Unit): TimedWork {
+        var iterationsSinceChange = 0
+        var totalIterations = 0
+        var minSecs = Int.MAX_VALUE
+        var maxSecs = Int.MIN_VALUE
+        while(iterationsSinceChange < maxIterationsNoChange) {
+            val input = setup()
+            val secs = time { work(input) }
+            iterationsSinceChange += 1; totalIterations += 1;
+            if(secs < minSecs) { minSecs = secs; iterationsSinceChange = 0; }
+            if(secs > maxSecs) { maxSecs = secs; iterationsSinceChange = 0; }
+        }
+        return TimedWork(minSecs, maxSecs, totalIterations)
+    }
 
     private fun List<Int>.printable(): String {
         val s = StringBuilder()
@@ -91,22 +139,35 @@ class MainActivity : AppCompatActivity() {
         return s.toString()
     }
 
-    /**
-     * A native method that is implemented by the 'jniplayground' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-    external fun sumC(nums: IntArray): Int
-    external fun sortC(nums: IntArray)
-    external fun plusOneC(nums: IntArray)
-    external fun startTime()
-    external fun initialize()
-    external fun endTime(): Double
-
     companion object {
+        private const val MAX_PRINT_SIZE = 90
+
         // Used to load the 'jniplayground' library on application startup.
         init {
             System.loadLibrary("jniplayground")
         }
+
+        @JvmStatic
+        external fun stringFromJNI(): String
+        @JvmStatic
+        external fun sumC(nums: IntArray): Int
+        @JvmStatic
+        external fun sortC(nums: IntArray)
+        @FastNative
+        @JvmStatic
+        external fun plusOneC(nums: IntArray)
+
+        @FastNative
+        @JvmStatic
+        external fun startTime()
+        @FastNative
+        @JvmStatic
+        external fun initialize()
+        @FastNative
+        @JvmStatic
+        external fun endTime(): Int
+        @FastNative
+        @JvmStatic
+        external fun clocksToSeconds(clocks: Int): Double
     }
 }
